7d68e983cb80d3eb522792e6c5a0743d177216cc
Akshay Kumar Akkinapelli <64968793+ak-akkinapelli@users.noreply.github.com>
2021-12-15 03:55:59 +0530

    Delete scripts directory

--- scripts/bcc.js
+++
@@ -1,7 +0,0 @@
-const randcolour = () => {
-  return Math.floor(Math.random() * 16777215).toString(16);
-};
-
-document.getElementById("button1").onclick = () => {
-  document.body.style.backgroundColor = randcolour();
-};
